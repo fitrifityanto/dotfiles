5a7d288bc3d193252fbf6eb7e96090bf13722571
fitnr <fitrifityanto@gmail.com>
2025-09-14 06:34:20 +0700

    feat: add nvzone-showkeys plugin for showing pressed keys

---
+++ nvim/lua/plugins/nvzone-showkeys.lua
@@ -0,0 +1,17 @@
+return {
+	"nvzone/showkeys",
+	cmd = "ShowkeysToggle",
+	opts = {
+		timeout = 1,
+		maxkeys = 5,
+		relative = "editor",
+		style = "minimal",
+		border = "single",
+		height = 1,
+		row = 1,
+		col = 0,
+		zindex = 100,
+		position = "top-right",
+		-- more opts
+	},
+}
